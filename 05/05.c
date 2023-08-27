c27022bb03ef489859f2fbc80d20a23b302a0929
Pritesh Suthar <124127811+priteshsuthar247@users.noreply.github.com>
2023-08-27 17:41:38 +0530

    Update 05.c

--- 05/05.c
+++ 05/05.c
@@ -1,6 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 int cqueue[10], cfront,crear;
+int squeue[10], sfront,srear;
+void senqueue()
+{
+    if (srear==4)
+    {
+        printf("\nQueue Overflow.\n");
+    }
+    else if (sfront&&srear==-1)
+    {
+        int temp;
+        sfront++;
+        srear++;
+        printf("Enter digit: ");
+        scanf("%d",&temp);
+        squeue[sfront]=squeue[srear]=temp;
+    }
+    else
+    {
+        srear++;
+        printf("Enter digit: ");
+        scanf("%d",&squeue[srear]);
+    }
+}
+void sdequeue()
+{
+    if (sfront==-1)
+    {
+        printf("\nQueue Underflow.\n");
+    }
+    else if (sfront==srear)
+    {
+        printf("\n%d is dequeued.\n",squeue[sfront]);
+        sfront=srear=-1;
+    }
+    else
+    {
+        printf("\n%d is dequeued.\n",squeue[sfront]);
+        sfront++;
+    }
+}
+void sdisplay()
+{
+    if (srear&&sfront==-1)
+    {
+        printf("\nQueue is empty.\n");
+    }
+    else
+    {
+        for (int i = sfront; i <=srear; i++)
+        {
+            printf("%d ",squeue[i]);
+        }
+    }
+    printf("\n");
+}
+void smenu()
+{
+    int choice;
+    restart:
+    printf("************************Simple Queue************************\n");
+    printf("\nPress 1 to Enqueue.\n");
+    printf("Press 2 to Dequeue.\n");
+    printf("Press 3 to Display.\n");
+    printf("Press 4 to Exit.\n");
+    printf("\nEnter your choice: ");
+    scanf("%d",&choice);
+    switch (choice)
+    {
+    case 1:
+        senqueue();
+        goto restart;
+    case 2:
+        sdequeue();
+        goto restart;
+    case 3:
+        sdisplay();
+        goto restart;
+    case 4:
+        printf("Exit.\n");
+        break;
+    default:
+        printf("Wrong Choice.\n");
+        goto restart;
+    }
+}
+void sinitialize()
+{
+    sfront=srear=-1;
+    smenu();
+}
 void cenqueue()
 {
     if ((crear+1)%5==cfront)
@@ -56,11 +146,13 @@ void cdisplay()
         }
         while (i != (crear + 1) % 5);
     }
+    printf("\n");
 }
 void cmenu()
 {
     int choice;
     restart:
+    printf("************************Circular Queue************************\n");
     printf("\nPress 1 to Enqueue.\n");
     printf("Press 2 to Dequeue.\n");
     printf("Press 3 to Display.\n");
@@ -90,10 +182,40 @@ void cmenu()
         break;
     }
 }
-int main()
+void cinitialize()
 {
     cfront=crear=-1;
     cmenu();
+}
+void mainmenu()
+{
+    int choice;
+    restart:
+    printf("************************Main Menu************************\n");
+    printf("\nPress 1 for Simple Queue.\n");
+    printf("Press 2 for Circular Queue.\n");
+    printf("Press 3 to Exit.\n");
+    printf("Enter your Choice: ");
+    scanf("%d",&choice);
+    switch (choice)
+    {
+    case 1:
+        sinitialize();
+        goto restart;
+    case 2:
+        cinitialize();
+        goto restart;
+    case 3:
+        printf("Exit.\n");
+        break;   
+    default:
+        printf("Wrong Choice.\n");
+        goto restart;
+    }
+}
+int main()
+{
+    mainmenu();
     system("PAUSE");
     return 0;
 }
