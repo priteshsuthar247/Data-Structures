e8c0f348c10a010eab80c3925b9e8278725909da
Pritesh Suthar <124127811+priteshsuthar247@users.noreply.github.com>
2023-09-27 11:24:23 +0530

    Add files via upload

---
+++ 11/11.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int a[5],low,mid,high,search;
+    printf("Enter: ");
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            int temp;
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+    printf("Search: ");
+    scanf("%d",&search);
+    low = 0, high = 4;
+    while (low <= high)
+    {
+        mid = (low + high)/2;
+        if (a[mid] == search)   
+        {
+            printf("Found: %d\n",a[mid]);
+        }
+        if (a[mid] < search)
+        {
+            low = mid+1;
+        }
+        else
+        {
+            high = mid-1;
+        }
+    }
+    system("PAUSE");
+    return 0;
+}
